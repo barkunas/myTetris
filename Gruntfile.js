368f704b6c0ec4e33e7ca453b133c6149b7dd7cb
Alexey Barkun <barkuns@gmail.com>
2019-08-27 14:47:36 +0300

    added .gitignore, gruntfile, package

---
+++ Gruntfile.js
@@ -0,0 +1,24 @@
+module.exports = function(grunt) {
+
+    // 1. Вся настройка находится здесь
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        concat: {
+            dist: {
+                src: [
+                    'src/*.js'  // Конкретный файл
+                ],
+                dest: 'dest/production.js',
+            }
+        }
+
+    });
+
+    // 3. Тут мы указываем Grunt, что хотим использовать этот плагин
+    grunt.loadNpmTasks('grunt-contrib-concat');
+
+    // 4. Указываем, какие задачи выполняются, когда мы вводим «grunt» в терминале
+    grunt.registerTask('default', ['concat']);
+
+};
