6b4c8d1d3e1bbc9ce7b48fbc7700e86489e6209a
Alexey Barkun <barkuns@gmail.com>
2019-10-31 16:37:15 +0300

    added gameLoop

--- public/js/controller.js
+++ public/js/controller.js
@@ -1,27 +1,43 @@
 class Controller {
     constructor() {
-        document.addEventListener('keyup', (event) => {
-            switch (event.code) {
-                case 'ArrowLeft':
-                    if (game.checkLeft()) game.moveItemLeft()
-                    console.log('toLeft');
-                    break;
-                case 'ArrowRight':
-                    if (game.checkRight()) game.moveItemRight()
-                    console.log('toRight');
-                    break;
-                case 'ArrowDown':
-                    if (game.checkDown()){ game.moveItemDown()}else{game.checkFullLine()}
-                    console.log('rotate');
-                    break;
-                case 'Space':
-                    if (game.checkRotate()) game.moveItemRotate()
-                    console.log('Down');
-                    break;
-            }
-        });
-        setInterval(()=>{
-            if (game.checkDown()){ game.moveItemDown()}else{game.checkFullLine()}
-        },1000)
+        this.status = true
+        this.gameLoop
+        this.initGameLoop()
+    }
+    gameEvents(event) {
+        switch (event.code) {
+            case 'ArrowLeft':
+                if (game.checkLeft()) game.moveItemLeft()
+                console.log('toLeft');
+                break;
+            case 'ArrowRight':
+                if (game.checkRight()) game.moveItemRight()
+                console.log('toRight');
+                break;
+            case 'ArrowDown':
+                if (game.checkDown()) { game.moveItemDown() } else { game.checkFullLine() }
+                console.log('rotate');
+                break;
+            case 'Space':
+                if (game.checkRotate()) game.moveItemRotate()
+                console.log('Down');
+                break;
+        }
+    }
+    initGameLoop(){
+        this.setGameEvents()
+        this.gameLoop = setInterval(() => {
+            if (game.checkDown()) { game.moveItemDown() } else { game.checkFullLine() }
+        }, 1000)
+    }
+    stopGameLoop(){
+        this.delGameEvents()
+        clearInterval(this.gameLoop)
+    }
+    setGameEvents(){
+        document.addEventListener('keyup', this.gameEvents);
+    }
+    delGameEvents(){
+        document.removeEventListener('keyup', this.gameEvents);
     }
 }
